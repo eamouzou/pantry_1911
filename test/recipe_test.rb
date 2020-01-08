8d95b758e151861fc588a1d6f9ce0d291f247cab
Elom Amouzou <elom1990@yahoo.com>
2020-01-08 10:11:31 -0700

    Tests pass for ingredient

---
+++ test/recipe_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+  def setup
+    @ingredient1 = Ingredient.new("Cheese", "C", 100)
+    @ingredient2 = Ingredient.new("Macaroni", "oz", 30)
+    @recipe = Recipe.new("Mac and Cheese")
+  end
+
+  def test_it_exists
+    assert_instance_of Recipe, @recipe
+  end
+
+  def test_name
+    
+  end
+end
