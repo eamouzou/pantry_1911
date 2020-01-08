6a64ac5f65ccec737b3b771205204cc150b5aa8f
Elom Amouzou <elom1990@yahoo.com>
2020-01-08 11:31:06 -0700

    create add recipe method and pass test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -7,10 +7,32 @@ require './lib/cook_book'
 class CookBookTest < Minitest::Test
   def setup
     @cookbook = CookBook.new
+    @ingredient1 = Ingredient.new("Cheese", "C", 100)
+    @ingredient2 = Ingredient.new("Macaroni", "oz", 30)
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @ingredient3 = Ingredient.new("Ground Beef", "oz", 100)
+    @ingredient4 = Ingredient.new("Bun", "g", 1)
+    @recipe2 = Recipe.new("Burger")
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 100)
   end
 
   def test_it_exists
     assert_instance_of CookBook, @cookbook
   end
+
+  def test_has_no_recipes
+    assert_equal [], @cookbook.recipes
+  end
+
+  def test_add_recipe
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
+
+
 
 end
