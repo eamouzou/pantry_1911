11738700e1361b57e28f6804fe0572a1faa69a92
Elom Amouzou <elom1990@yahoo.com>
2020-01-08 10:34:48 -0700

    created stock method and all tests pass

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,9 +7,18 @@ require './lib/pantry'
 class PantryTest < Minitest::Test
   def setup
     @pantry = Pantry.new
+    @ingredient1 = Ingredient.new("Cheese", "C", 50)
+    @ingredient2 = Ingredient.new("Macaroni", "oz", 200)
+    @recipe = Recipe.new("Mac and Cheese")
+    @recipe.add_ingredient(@ingredient1, 2)
+    @recipe.add_ingredient(@ingredient2, 8)
   end
 
   def test_it_exists
     assert_instance_of Pantry, @pantry
+  end
+
+  def test_stock
+    assert_equal ({}), @pantry.stock
   end
 end
